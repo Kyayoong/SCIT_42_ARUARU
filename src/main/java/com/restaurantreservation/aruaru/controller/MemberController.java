819fe9f515606375411dd446e827840666a361b1
SoungJun Cho <gksrnr0529@gmail.com>
2022-08-30 16:31:44 +0900

    none

--- src/main/java/com/restaurantreservation/aruaru/controller/MemberController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/MemberController.java
@@ -2,7 +2,9 @@ package com.restaurantreservation.aruaru.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("member")
 @Controller
 public class MemberController {
 	@GetMapping("login")

e148be41c19f2abdff4e33d147f2f2f2e3f48a37
Yehyun <yehyun20124@gmail.com>
2022-09-05 20:06:28 +0900

    수정완

--- src/main/java/com/restaurantreservation/aruaru/config/WebSecurityConfig.java
+++ src/main/java/com/restaurantreservation/aruaru/config/WebSecurityConfig.java
@@ -1,12 +1,74 @@
-//package com.restaurantreservation.aruaru.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//public class WebSecurityConfig {
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//	}
-//}
+package com.restaurantreservation.aruaru.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Security 설정
+ */
+@Configuration
+public class WebSecurityConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    //설정
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/",
+        		"/joinselect",
+        		"/join_as_user",
+        		"/insert_user",
+                "/css/**",
+                "/images/**",
+                "/js/**").permitAll()		//설정한 리소스의 접근을 인증절차 없이 허용
+        .anyRequest().authenticated()   	//위의 경로 외에는 모두 로그인을 해야 함
+        .and()
+        .formLogin()						//일반적인 폼을 이용한 로그인 처리/실패 방법을 사용
+        .loginPage("/loginForm")		//시큐리티에서 제공하는 기본 폼이 아닌 사용자가 만든 폼 사용
+        .loginProcessingUrl("/login").permitAll()	//인증 처리를 하는 URL을 설정. 로그인 폼의 action으로 지정
+        .usernameParameter("member_id")		//로그인폼의 아이디 입력란의 name
+        .passwordParameter("member_pw")		//로그인폼의 비밀번호 입력란의 name
+        .and()
+        .logout()
+        .logoutSuccessUrl("/").permitAll()	//로그아웃시에 이동할 경로
+        .and()
+        .cors()
+        .and()
+        .httpBasic();
+
+        return http.build();
+    }
+
+    //인증을 위한 쿼리
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        // 인증 (로그인)
+        .usersByUsernameQuery(
+        		"select member_id username, member_pw password, enabled " +
+                "from user_member " +
+                "where member_id = ?")
+        // 권한
+        .authoritiesByUsernameQuery(
+        		"select member_id username, member_role role_name " +
+                "from user_member " +
+                "where member_id = ?");
+    }
+
+    // 단방향 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}

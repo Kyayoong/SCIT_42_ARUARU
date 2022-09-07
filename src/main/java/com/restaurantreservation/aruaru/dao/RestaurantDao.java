2915b926b77edd8326730e49fa34d1ee6dcfe007
ryuki <ryukiwon001@gmail.com>
2022-09-07 18:31:10 +0900

    sdsd

--- src/main/java/com/restaurantreservation/aruaru/dao/RestaurantDao.java
+++ src/main/java/com/restaurantreservation/aruaru/dao/RestaurantDao.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface RestaurantDao {
 	
+	int restaurantInsert();
+	
+	
 	
 
 }
--- src/main/java/com/restaurantreservation/aruaru/service/RestaurantServiceImpl.java
+++ src/main/java/com/restaurantreservation/aruaru/service/RestaurantServiceImpl.java
@@ -1,7 +1,10 @@
 package com.restaurantreservation.aruaru.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.restaurantreservation.aruaru.dao.RestaurantDao;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,5 +12,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Transactional
 public class RestaurantServiceImpl implements RestaurantService {
-
+	
+	@Autowired
+	RestaurantDao dao;
+	
+	public int restaurantInsert() {
+		int result = dao.restaurantInsert();
+		return result;
+	}
+	
+	
+	
 }

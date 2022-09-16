ee3b24bd110564c148a3875c5601bb87a6f06f14
ryuki <ryukiwon001@gmail.com>
2022-09-16 16:42:07 +0900

    ryuki

--- src/main/java/com/restaurantreservation/aruaru/controller/RestaurantRestController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/RestaurantRestController.java
@@ -1,8 +1,19 @@
 package com.restaurantreservation.aruaru.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.restaurantreservation.aruaru.domain.Menu;
+import com.restaurantreservation.aruaru.domain.Restaurant_member;
+import com.restaurantreservation.aruaru.service.RestaurantService;
+import com.restaurantreservation.aruaru.util.FileService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,5 +22,32 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @ResponseBody
 public class RestaurantRestController {
+	
+	/**
+	 * 게시판 첨부파일 업로드 경로
+	 */
+	@Value("${spring.servlet.multipart.location}")
+	String uploadPath;
+	
+	@Autowired
+	RestaurantService service;
+	
+	@PostMapping("insertmenu")
+	public void insertmenu(MultipartFile upload,Menu menu,@AuthenticationPrincipal UserDetails user) {
+		log.debug("업로드 {}",upload);
+		log.debug("메뉴 {}",menu);
+		
+		Restaurant_member member = service.selectOne(user.getUsername());
+		
+		String savedfile = FileService.saveFile(upload, uploadPath);
+		menu.setRestaurant_num(member.getRestaurant_num());
+		menu.setMenu_originalfile(upload.getOriginalFilename());
+		menu.setMenu_savedfile(savedfile);
+		menu.setMenu_suggestion(menu.getMenu_suggestion());
+		
+		int result = service.insertmenu(menu);
+		
+		log.debug("결과 {}",result);
+	}
 	
 }
--- src/main/java/com/restaurantreservation/aruaru/dao/RestaurantDao.java
+++ src/main/java/com/restaurantreservation/aruaru/dao/RestaurantDao.java
@@ -2,17 +2,24 @@ package com.restaurantreservation.aruaru.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.restaurantreservation.aruaru.domain.Menu;
 import com.restaurantreservation.aruaru.domain.Restaurant_file;
 import com.restaurantreservation.aruaru.domain.Restaurant_member;
 
 @Mapper
 public interface RestaurantDao {
 	
+	//아이디로 조회하여 식당객체가져오기
 	Restaurant_member selectOne(String Member_id);
-	
+		
+	//식당회원 저장하기
 	int regist1(Restaurant_member member);
-	
+		
+	//식당파일 저정하기
 	int fileregist(Restaurant_file file);
+		
+	//메뉴 저장하기
+	int insertmenu(Menu menu);
 	
 	
 	

aee2ff56cad2b421ec8b501064f7124494c96eb5
ryuki <ryukiwon001@gmail.com>
2022-09-17 21:25:53 +0900

    dummy

--- src/main/java/com/restaurantreservation/aruaru/domain/Restaurant_member.java
+++ src/main/java/com/restaurantreservation/aruaru/domain/Restaurant_member.java
@@ -13,13 +13,14 @@ public class Restaurant_member {
 	int restaurant_num;				// 가게번호
 	String member_id; 				// 회원아이디
 	String restaurant_name; 		// 가게이름
-	String restaurant_ename; 		// 가게영어 이름
 	String restaurant_sectors; 		// 업종선택
 	String restaurant_comment; 		// 상세설명
 	String restaurant_phone; 		// 가게번호
 	int restaurant_grade; 			// 평점
 	String registration_date;		// 등록일
-	String restaurant_address; 		// 가게 실제주소4
+	String restaurant_address1; 	// 도로명
+	String restaurant_address2; 	// 상세주고
+	String restaurant_addressinfo; 	// 오는길 설명
 	String menu_priceavr;			// 1인당 평균 가격
 	int certification; 				// 허가 여부
 }

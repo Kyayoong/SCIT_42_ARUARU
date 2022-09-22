cc65080253c980e6178c5c42e97fcca4c091c6ae
Yehyun <yehyun20124@gmail.com>
2022-09-22 09:29:44 +0900

    닉네임가져오기

--- src/main/java/com/restaurantreservation/aruaru/controller/HomeController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/HomeController.java
@@ -1,23 +1,37 @@
 package com.restaurantreservation.aruaru.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.restaurantreservation.aruaru.domain.User_member;
 import com.restaurantreservation.aruaru.domain.tabletest;
 import com.restaurantreservation.aruaru.service.UserService;
 
 @Controller
 public class HomeController {
-	//오라클 연결 테스트용
-	@Autowired UserService service;
-	
-	@GetMapping({"","/"})
-	public String home() {
+	// 오라클 연결 테스트용
+	@Autowired
+	UserService service;
+
+	@GetMapping({ "", "/" })
+	public String home(Model model, @AuthenticationPrincipal UserDetails user) {
+		
+		if(user != null) {
+			User_member member = service.selectUser(user.getUsername());
+			model.addAttribute("member_nickname", member.getMember_nickname());
+		}
+		else {
+			model.addAttribute("member_nickname", null);
+		}
+
 		return "home";
 	}
-	
+
 	/*
 	 * //오라클 연결 테스트용
 	 * 
--- src/main/java/com/restaurantreservation/aruaru/controller/MyPageController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/MyPageController.java
@@ -1,15 +1,33 @@
 package com.restaurantreservation.aruaru.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.restaurantreservation.aruaru.domain.User_member;
+import com.restaurantreservation.aruaru.service.UserService;
 
 @RequestMapping("mypage")
 @Controller
 public class MyPageController {
+	@Autowired
+	UserService service;
+	
 	// 마이페이지 메인화면
 	@GetMapping("/")
-	public String mypage() {
+	public String mypage(Model model, @AuthenticationPrincipal UserDetails user) {
+
+		if (user != null) {
+			User_member member = service.selectUser(user.getUsername());
+			model.addAttribute("member_nickname", member.getMember_nickname());
+		} else {
+			model.addAttribute("member_nickname", null);
+		}
+
 		return "userView/mypage";
 	}
 

d6e2d305fb56ee77601cf4586b677221ffda39f3
KyeongHwanHwang <ghkdrudghks1@naver.com>
2022-09-26 16:35:25 +0900

    회원탈퇴(leaveid) html 및 css 추가

--- src/main/java/com/restaurantreservation/aruaru/controller/ChatController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/ChatController.java
@@ -1,7 +1,10 @@
 package com.restaurantreservation.aruaru.controller;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +16,28 @@ public class ChatController {
 	public ModelAndView chat(Model m) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("chat");
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("E:\\workspace\\Test_SpringBoot\\file.txt"));
+			String text = "";
+			String a1;
+			try {
+				while ((a1 = in.readLine()) != null) {
+				  StringBuffer sb = new StringBuffer(a1);
+				 text+=sb.toString()+"\n";
+				 m.addAttribute("chatDetail",text);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return mv;
 	}
+	
+	
 	
 	
 }

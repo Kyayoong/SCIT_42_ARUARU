f022bf8bb439927cf30709cff19b596a2c00d320
ryuki <ryukiwon001@gmail.com>
2022-09-28 18:23:40 +0900

    re

--- src/main/java/com/restaurantreservation/aruaru/domain/Reservation.java
+++ src/main/java/com/restaurantreservation/aruaru/domain/Reservation.java
@@ -10,11 +10,10 @@ import lombok.NoArgsConstructor;
 public class Reservation {
 	
 	
-    int reservation;			//예약 번호
+    int reservation_num;		//예약 번호
     String member_id;			//회원아이디
     int restaurant_num;			//식당번호
     int reservation_people;		//예약인수
-    String reservation_seat;	//좌석 종류
     String registration_date;	//등록일
     String reservation_date;	//예약일
     String reservation_hours;	//예약시간

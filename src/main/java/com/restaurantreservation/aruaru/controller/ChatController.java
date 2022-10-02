76bce306021f65bccd16a9a36e7451cbcdd44f68
KyeongHwanHwang <ghkdrudghks1@naver.com>
2022-10-02 15:35:30 +0900

    채팅 저장 경로 수정 (Handler 부분)

--- src/main/java/com/restaurantreservation/aruaru/controller/ChatController.java
+++ src/main/java/com/restaurantreservation/aruaru/controller/ChatController.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -12,23 +13,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.restaurantreservation.aruaru.domain.User_member;
+import com.restaurantreservation.aruaru.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Controller
 public class ChatController {
+	@Autowired
+	UserService service;
+	
 	@GetMapping("/chat")
 	public ModelAndView chat(Model m, @AuthenticationPrincipal UserDetails user) {
+		log.debug("1");
 		ModelAndView mv = new ModelAndView();
+		log.debug("2");
 		mv.setViewName("chat");
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("C:\\upload\\file.txt"));
 			String text = "";
 			String a1;
+			log.debug("3");
 			try {
 				while ((a1 = in.readLine()) != null) {
 				  StringBuffer sb = new StringBuffer(a1);
 				 text+=sb.toString()+"\n";
 				 m.addAttribute("chatDetail", text);
+				 log.debug("4");
 				 if(user != null) {
-					 m.addAttribute("chatName", user.getUsername());	 
+					 log.debug("5");
+					 User_member member = service.selectUser(user.getUsername());
+					 log.debug("6");
+					 m.addAttribute("chatName",member.getMember_id());
+					 log.debug("7");
+					 log.debug("{}", member.getMember_id());
+					 
 				 }
 				}
 			} catch (IOException e) {
